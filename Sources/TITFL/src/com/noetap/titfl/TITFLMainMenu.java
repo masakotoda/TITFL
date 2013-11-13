3062e64d602c10e1da2e48de9ba373672b2cf73f
tonythaioc <tonythaioc@yahoo.com>
2013-11-12 21:35:07 -0800

    Revert "Add TITFLMainMemu() constructor to fix compiling error"
    This reverts commit 7530612f3c45e05381e233840e48b5766c851434.

--- Sources/TITFL/src/com/noetap/titfl/TITFLMainMenu.java
+++ Sources/TITFL/src/com/noetap/titfl/TITFLMainMenu.java
@@ -3,10 +3,6 @@ package com.noetap.titfl;
 public class TITFLMainMenu {
 	private int numOfPlayer = -1;
 
-	public TITFLMainMenu()
-	{
-		numOfPlayer = -1;
-	}
 	public int getNumOfPlayer() {
 		return numOfPlayer;
 	}
