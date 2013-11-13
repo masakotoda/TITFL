7530612f3c45e05381e233840e48b5766c851434
tonythaioc <tonythaioc@yahoo.com>
2013-11-12 18:36:58 -0800

    Add TITFLMainMemu() constructor to fix compiling error

--- Sources/TITFL/src/com/noetap/titfl/TITFLMainMenu.java
+++ Sources/TITFL/src/com/noetap/titfl/TITFLMainMenu.java
@@ -3,6 +3,10 @@ package com.noetap.titfl;
 public class TITFLMainMenu {
 	private int numOfPlayer = -1;
 
+	public TITFLMainMenu()
+	{
+		numOfPlayer = -1;
+	}
 	public int getNumOfPlayer() {
 		return numOfPlayer;
 	}
